a710a21d935aa3d7c995e58212388a32e3f430ff
lichunyan <2678402707@qq.com>
2018-07-04 17:01:54 +0800

    组件

--- Vue-Project/src/components/views/swipe.vue
+++ Vue-Project/src/components/views/swipe.vue
@@ -1,24 +1,37 @@
 <template>
     <div class="swiper">
-       <mt-swipe :show-indicators="true">
-        <mt-swipe-item>
-                <img src="http://img05.tooopen.com/images/20150820/tooopen_sy_139205349641.jpg" alt="">  
-        </mt-swipe-item>
-        <mt-swipe-item>
-            <img src="http://img.zcool.cn/community/01d881579dc3620000018c1b430c4b.JPG@3000w_1l_2o_100sh.jpg" alt="">
-        </mt-swipe-item>
-        <mt-swipe-item>
-            
-            <img src="http://img.zcool.cn/community/01690955496f930000019ae92f3a4e.jpg@2o.jpg" alt=""></mt-swipe-item>
-       </mt-swipe> 
+        <!-- woshi  -->
+       <div class="swiper-wrap">
+           <div class="swiper-content">
+               <div class="img-wrap">
+               <img src="http://img05.tooopen.com/images/20150820/tooopen_sy_139205349641.jpg" alt="">
+                </div>
+                <div class="img-wrap">
+                    <img src="http://img.zcool.cn/community/018d4e554967920000019ae9df1533.jpg@900w_1l_2o_100sh.jpg" alt="">
+                </div>
+                <div class="img-wrap">
+                    
+                    <img src="http://img.zcool.cn/community/01690955496f930000019ae92f3a4e.jpg@2o.jpg" alt="">
+                </div>
+           </div>
+       </div>
     </div>
 </template>
-<style scope lang="css">
-.swiper {
-    height: 300px;
-}
-img {
-    height: 100%;
-    width: 100%;
+<style scoped lang="less">
+.swiper-wrap {
+    
+    width: 10000px;
+    height: 500px;
+    overflow: hidden;
+    .swiper-content {
+        width: 1000px;
+        overflow: hidden;
+    }
+    .img-wrap {
+        float: left;
+        height: 100%;
+        
+
+    }
 }
 </style>
